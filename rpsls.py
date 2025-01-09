a2e3049c444275b7e4f754b4330c9def7ecfb478
R4JV12 <tropicalEcho+github@proton.me>
2025-01-09 11:58:43 +0530

    batman and robin

--- rpsls.py
+++ rpsls.py
@@ -1,9 +1,10 @@
 import random, sys, os, time; CHARACTERS = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK"]; COMMANDS = ["CLEAR", "CLS", "EXIT", "QUIT", "HELP", "H", "MANUAL", "GUIDE", "SHELDON"]; WINS = {"ROCK": ["SCISSORS", "LIZARD"], "PAPER": ["ROCK", "SPOCK"], "SCISSORS": ["PAPER", "LIZARD"], "LIZARD": ["PAPER", "SPOCK"], "SPOCK": ["SCISSORS", "ROCK"]}; regularHelp = """COMMANDS:\nCLEAR | CLS                  CLEARS THE SCREEN\nEXIT | QUIT                  KILLS THE GAME\nHELP | H                     PRINTS THIS\nMANUAL | GUIDE | SHELDON     SUMMONS SHELDON FOR HELP\n\nGUIDE:\nENTER ( SINGLE | SINGLEPLAYER | 1 | ONE | MONO ) IF YOU WANNA PLAY AGAINST COMPUTER\nENTER ( MULTI | MULTIPLAYER | 2 | TWO | BI | DUO ) IF YOU WANNA PLAY AGAINST A HUMAN""" ; sheldonHelp = """SCISSORS cuts PAPER\nPAPER covers ROCK\nROCK crushes LIZARD\nLIZARD poisons SPOCK\nSPOCK smashes SCISSORS\nSCISSORS decapitates LIZARD\nLIZARD eats PAPER\nPAPER disproves SPOCK\nSPOCK vaporizes ROCK\nand as it always has\nROCK crushes SCISSORS"""
 def clear(): os.system("cls" if os.name == "nt" else "clear")
-def handleCommands(c): clear() if c in ["CLEAR", "CLS"] else sys.exit("GOODBYE!") if c in ["EXIT", "QUIT"] else print(HELP) if c in ["HELP", "H"] else print(SHELDON) if c in ["MANUAL", "GUIDE", "SHELDON"] else ""; return c in COMMANDS
+def handleCommands(c): clear() if c in ["CLEAR", "CLS"] else sys.exit("GOODBYE!") if c in ["EXIT", "QUIT"] else print(regularHelp) if c in ["HELP", "H"] else print(sheldonHelp) if c in ["MANUAL", "GUIDE", "SHELDON"] else None; return c in COMMANDS
 def getChoice(p): 
     while True: 
-        if c := input(p).strip().upper() in CHARACTERS: return c
+        c = input(p).strip().upper()
+        if c in CHARACTERS: return c
         elif handleCommands(c): continue
         else: print("INVALID CHOICE!")
 def playGame(p1, p2, s): 
